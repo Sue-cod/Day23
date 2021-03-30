e71c7451f32563d48b86c8174ae38edc6fd3ef7b
Sue-cod <snozic892@gmail.com>
2021-03-30 20:10:43 +0200

    prvi

---
+++ ex01/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Plant from './Plant';
+
+function App() {
+  return (
+    <div idName="App">
+     <Plant />
+    </div>
+  );
+}
+export default App;
